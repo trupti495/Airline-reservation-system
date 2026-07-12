4335aa95fd575a3431809fff1a575f3b961077d8
pranjal0641 <pranjalbochare@gmail.com>
2026-07-12 11:07:05 +0530

    Completed Flight Managementmodule

--- database.py
+++ database.py
@@ -1,6 +1,8 @@
 import sqlite3
 
 def get_connection():
-    conn = sqlite3.connect("airline.db")
+    conn = sqlite3.connect(
+        r"C:\Users\Pranjal\OneDrive\Desktop\Airline-reservation-sytem\airline.db"
+    )
     conn.execute("PRAGMA foreign_keys = ON")
     return conn
